c3658eaeb8190bd0aa8219e24c518df1e5f35d2b
roozbehsaghi <roozbehsaghi@gmail.com>
2018-05-09 11:15:06 +0200

    easy Level

--- test/MijnAbstractClas.java
+++ test/MijnAbstractClas.java
@@ -4,6 +4,10 @@ public abstract class MijnAbstractClas {
 	
 	public abstract String method();
 	
+	public abstract int age();
+	
+	protected abstract float doFloat();
+	
 	public static String method2() {
 		
 		return "method2";
--- test/Test.java
+++ test/Test.java
@@ -5,11 +5,20 @@ public class Test extends MijnAbstractClas{
 	public static void main(String[] args) {
 		
 	}
+	public int age() {
+		
+		return 2;
+	}
 
 	@Override
 	public String method() {
 		// TODO Auto-generated method stub
 		return "text";
+	}
+	@Override
+	protected float doFloat() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
